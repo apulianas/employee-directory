ae435fd61b2f431119d14738510867d82b37b7f1
“Alec <apulianas@gmail.com>
2021-11-02 20:19:44 -0400

    update .gitignore

--- react-frontend/src/pages/Create.js
+++ react-frontend/src/pages/Create.js
@@ -1,79 +1,73 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
-class CreatePage extends Component {
-    constructor(props) {
-        super(props);
-        this.nameElement = React.createRef();
-        this.titleElement = React.createRef();
-        this.locationElement = React.createRef();
-        this.emailElement = React.createRef();
-        this.phoneElement = React.createRef();
-        this.pictureElement = React.createRef();
-        this.submitHandler = this.submitHandler.bind(this);
-    }
+function CreatePage() {
+    const [name, setName] = useState('');
+    const [title, setTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [picture, setPicture] = useState('');
 
-    submitHandler = event => {
+    const handleNameChange = event => {
+        setName(event.target.value)
+    };
+    const handleTitleChange = event => {
+        setTitle(event.target.value)
+    };
+    const handleLocationChange = event => {
+        setLocation(event.target.value)
+    };
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    };
+    const handlePhoneChange = event => {
+        setPhone(event.target.value)
+    };
+    const handlePictureChange = event => {
+        setPicture(event.target.value)
+    };
+
+    const handleSubmit = event => {
         event.preventDefault();
-        const name = this.nameElement.current.value;
-        const title = this.titleElement.current.value;
-        const location = this.locationElement.current.value;
-        const email = this.emailElement.current.value;
-        const phone = this.phoneElement.current.value;
-        const picture = this.pictureElement.current.value;
-
-        
-        /*const requestBody = {
+        fetch('http://localhost:8000/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
             query: `
                 mutation {
-                    createEmployee(employeeInput: {name: "${name}", title: "${title}", location: "${location}", email: "${email}", phone: "${phone}", picture: "${picture}") {
+                    createEmployee (
+                    employeeInput: {name: "${name}", title: "${title}", location: "${location}", email: "${email}", phone: "${phone}", picture: "${picture}"}) {
                         _id
                         name
                     }
-                }
-            `
-        };*/
-
-        fetch('http://localhost:8000/graphql', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                query: `
-        mutation {
-            createEmployee (
-            employeeInput: {name: "${name}", title: "${title}", location: "${location}", email: "${email}", phone: "${phone}", picture: "${picture}"}) {
-                _id
-                name
-            }
-        }`,
-            }),
-        })
-        .then((res) => res.json())
-        .then((result) => console.log(result));
+    }`,
+        }),
+    })
+    .then((res) => res.json())
+    .then((result) => console.log(result));
+    };
         
-    }
-    render() {
-        return (
-            <form>
-                <div className="form-control" onSubmit={this.submitHandler}>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" value={this.nameElement} />
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" value={this.titleElement} />
-                    <label htmlFor="location">Location</label>
-                    <input type="text" id="location" value={this.locationElement} />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" value={this.emailElement} />
-                    <label htmlFor="phone">Phone</label>
-                    <input type="phone" id="phone" value={this.phoneElement} />
-                    <label htmlFor="text">Picture</label>
-                    <input type="picture" id="picture" value={this.pictureElement} />
-                </div>
-                <div className="form-actions">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        );
-    }
+    return (
+        <form onSubmit = {handleSubmit}>
+            <div>
+                <label>Name</label>
+                <input type="text" id="name" onChange={handleNameChange} value={name} />
+                <label>Title</label>
+                <input type="text" id="title" onChange={handleTitleChange} value={title}/>
+                <label>Location</label>
+                <input type="text" id="location" onChange={handleLocationChange} value={location}/>
+                <label>Email</label>
+                <input type="email" id="email" onChange={handleEmailChange} value={email}/>
+                <label>Phone</label>
+                <input type="phone" id="phone" onChange={handlePhoneChange} value={phone}/>
+                <label>Picture</label>
+                <input type="picture" id="picture" onChange={handlePictureChange} value={picture}/>
+            </div>
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    );
 }
 
 export default CreatePage;
