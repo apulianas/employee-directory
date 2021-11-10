6a8b4d315162a1834efcb5b8843d1097b3a76494
“Alec <apulianas@gmail.com>
2021-11-09 23:18:02 -0500

    Improved some UI and played with getting data from GraphQL with mixed success

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import DirectoryPage from './pages/Directory';
 import CreatePage from './pages/Create';
 import './App.css';
 
+import {ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client";
 
-class App extends Component{
+const App = () => {
+  //grab contacts from database 
+  const client = new ApolloClient({
+    uri: "http://localhost:8000/graphql",
+    cache: new InMemoryCache()
+  });
+  
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch> 
+          <Redirect from="/" to="/directory" exact />
+          <Route path="/directory" component={DirectoryPage} />
+          <Route path="/create" component={CreatePage} />
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider> 
+   
+  );
+}
+
+/*class App extends Component{
   render (){
     return (
       <BrowserRouter>
@@ -18,6 +40,6 @@ class App extends Component{
       </BrowserRouter>
     );
   }
-}
+}*/
 
 export default App;
