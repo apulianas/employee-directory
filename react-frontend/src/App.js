0904bc89efa9084462df7e574b536fa733be250a
“Alec <apulianas@gmail.com>
2021-11-10 21:39:31 -0500

    trying to iterate over employee data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,45 +3,22 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import DirectoryPage from './pages/Directory';
 import CreatePage from './pages/Create';
+import AdminPage from './pages/Admin';
 import './App.css';
 
-import {ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client";
-import AdminPage from './pages/Admin';
 
 const App = () => {
-  //grab contacts from database 
-  const client = new ApolloClient({
-    uri: "http://localhost:8000/graphql",
-    cache: new InMemoryCache()
-  });
-  
   return (
-    <ApolloProvider client={client}>
       <BrowserRouter>
         <Switch> 
-          <Redirect from="/" to="/directory" exact />
+          <Redirect from="/" to="/directory" exact /> 
           <Route path="/directory" component={DirectoryPage} />
           <Route path="/create" component={CreatePage} />
           <Route path="/admin" component={AdminPage} />
         </Switch>
       </BrowserRouter>
-    </ApolloProvider> 
    
   );
 }
-
-/*class App extends Component{
-  render (){
-    return (
-      <BrowserRouter>
-        <Switch> 
-          <Redirect from="/" to="/directory" exact />
-          <Route path="/directory" component={DirectoryPage} />
-          <Route path="/create" component={CreatePage} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}*/
 
 export default App;
--- react-frontend/src/ContactCell.js
+++ react-frontend/src/ContactCell.js
@@ -1,51 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { useQuery, gql } from "@apollo/client";
+import React from "react";
 
-function ContactCell() {
-    /*
-    const EMPLOYEE_QUERY = gql`
-    query{
-        employees {
-        name
-        title
-        email
-        phone
-        location
-        picture
-        }
-    }`;
-
-    const {data, loading, error} = useQuery(EMPLOYEE_QUERY);
-    const [employees, setEmployees] = useState([]);
-    useEffect (() => {
-    if (data) {
-        setEmployees(data.employees);
-    }
-    }, [data]);*/
-
+function ContactCell(employeeData) {
+    console.log(employeeData)
     return (
         <>
-        <figure className="bg-white h-80 rounded-md pt-6">
+        {employeeData?.map((employee, index) => (
+            <figure className="bg-white h-80 rounded-md pt-6" key={index}>
             <img 
                 alt="user"
                 className="w-16 h-16 rounded-full mx-auto"
-                src="https://randomuser.me/api/portraits/women/15.jpg"
+                src={employee.picture}
             /> 
             <figcaption className="text-center mt-5">
             <p className="text-gray-700 font-semibold text-xl mb-2">
-              Alec
+              {employee.name}
             </p>
             <p className="text-gray-500">
-              <span className="font-medium">email: </span>apulianas@gmail.com
+              <span className="font-medium">title: </span>{employee.title}
             </p>
             <p className="text-gray-500">
-              <span className="font-medium">phone: </span>410-409-7392
+              <span className="font-medium">email: </span>{employee.email}
             </p>
             <p className="text-gray-500">
-              <span className="font-medium">city: </span> Baltimore
+              <span className="font-medium">phone: </span>{employee.phone}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">city: </span> {employee.location}
             </p>
             </figcaption>
-        </figure>
+            </figure>
+        ))}
         </>
     )
 }
